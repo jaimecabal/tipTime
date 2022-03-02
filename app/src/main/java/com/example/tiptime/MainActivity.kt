bdacf9e9fec863e4ac8cfa8ad325e20cea5f8963
jcabal <jcabalf3@gmail.com>
2022-03-02 19:20:39 +0100

    Como cambiar el tema de una app

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -6,37 +6,8 @@ import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    //Funcion principal
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.calculateButton.setOnClickListener{
-            calculateTip()
-        }
-    }
-    //Funcion calcular propina
-    private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDoubleOrNull()
-        if (cost == null) {
-            binding.tipResult.text = ""
-            return
-        }
-
-        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-            R.id.option_twenty_percent -> 0.20
-            R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
-        }
-
-        var tip = tipPercentage * cost
-        if (binding.roundUpSwitch.isChecked) {
-            tip = kotlin.math.ceil(tip)
-        }
-
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+        setContentView(R.layout.activity_main)
     }
 }
